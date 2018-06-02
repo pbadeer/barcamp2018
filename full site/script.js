9ec31bc4cf19b4e5228a93f3b8c7d83fb1e92640
Preston Badeer <preston@badeer.co>
2018-06-02 13:27:25 -0500

    progress on full site

--- full site/script.js
+++ full site/script.js
@@ -2,15 +2,6 @@ window.addEventListener("load", init);
 window.addEventListener("resize", init);
 
 function init(e) {
-  document.querySelectorAll('.bc18_lettermark_a, .bc18_lettermark_b')
-    .forEach(function(elem) {
-      elem.addEventListener('mouseover', function(){
-        document.querySelectorAll('.egg').forEach(function(elem) {
-          elem.classList.add('expand');
-        });
-      });
-  });
-
   function findAncestor (el, sel) {
     while ((el = el.parentElement) && !((el.matches || el.matchesSelector).call(el,sel)));
     return el;
@@ -18,7 +9,6 @@ function init(e) {
   
   var draggableElems = document.querySelectorAll('.drag');
   var draggies = [];
-  var editor = document.querySelector('[contenteditable]');
 
   // MOBILE
   if (window && window.outerWidth <= 768) {
@@ -32,26 +22,16 @@ function init(e) {
         draggableElems[i].setAttribute('style', '');
       
     }
-    // Disable notepad editing on mobile
-    editor.setAttribute('contenteditable', false);
   }
 
   // NOT MOBILE
   else {
-    // Keep links clickable despite editable note
-    editor.addEventListener('click', function(e) {
-      if (e.target.tagName == 'A') {
-        window.open(e.target.getAttribute('href'));
-      }
-    });
     // Make windows draggable if not mobile
     for ( var i=0; i < draggableElems.length; i++ ) {
       var backup = draggableElems[i].dataset.style;
       if (backup) {
         draggableElems[i].setAttribute('style',backup);
       }
-      // enable editing
-      editor.setAttribute('contenteditable', true);
 
       var draggableElem = draggableElems[i];
       var draggie = new Draggabilly( draggableElem, {
