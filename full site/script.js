49daed68d99ea0ba9e187040ef2e2dbb69839629
Preston Badeer <preston@badeer.co>
2018-06-03 03:44:08 -0500

    styling for almost everything, basic window opening, links

--- full site/script.js
+++ full site/script.js
@@ -1,7 +1,37 @@
 window.addEventListener("load", init);
 window.addEventListener("resize", init);
 
+function startTime() {
+  var today = new Date();
+  var d = today.getDate();
+  var mo = (today.getMonth() + 1);
+  var y = today.getFullYear();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  d = checkTime(d);
+  mo = checkTime(mo);
+  m = checkTime(m);
+  s = checkTime(s);
+  document.querySelector('.clock').innerHTML =
+  mo + "/" + d + "/" + y + " " + h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+function openWindow (a) {
+  var b = document.querySelectorAll('.' + a)[0];
+  b.style.top = '25vh';
+  b.style.opacity = 1;
+  b.style.transform = '';
+}
+
 function init(e) {
+  startTime();
+
   function findAncestor (el, sel) {
     while ((el = el.parentElement) && !((el.matches || el.matchesSelector).call(el,sel)));
     return el;
@@ -37,9 +67,9 @@ function init(e) {
       var draggie = new Draggabilly( draggableElem, {
         handle: '.handle'
       });
-      // draggie.on('dragStart', function(event, pointer) {
-      //   findAncestor(event.target, ".drag").classList.add('moved');
-      // });
+      draggie.on('dragStart', function(event, pointer) {
+        findAncestor(event.target, ".drag").classList.remove('hidden');
+      });
       draggies.push( draggie );
     }
   }
